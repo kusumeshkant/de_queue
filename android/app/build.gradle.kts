bdde28b87f57dafecdeba9418d828564aaf4e32f
kusumeshkant <kushumeshkant2013@gmail.com.com>
2026-02-01 12:36:51 +0530

    corrected buid file for firebase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     // the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.4" apply false
-    id("com.android.application")
+   // id("com.google.gms.google-services") version "4.4.4" apply false
    // the Google services Gradle plugin
     id("com.google.gms.google-services")
 
